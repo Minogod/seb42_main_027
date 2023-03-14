cf5a4393743750333f1e376aa9a841bf370eb57c
Minogod <93550624+Minogod@users.noreply.github.com>
2023-03-14 23:06:09 +0900

    feat: 합친거

--- server/build/generated/sources/annotationProcessor/java/main/ynzmz/server/comment/lecturereview/mapper/LectureReviewPostCommentMapperImpl.java
+++ server/build/generated/sources/annotationProcessor/java/main/ynzmz/server/comment/lecturereview/mapper/LectureReviewPostCommentMapperImpl.java
@@ -9,7 +9,7 @@ import ynzmz.server.comment.lecturereview.entity.LectureReviewComment;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-03-14T15:57:40+0900",
+    date = "2023-03-14T23:00:52+0900",
     comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.1.jar, environment: Java 11.0.17 (Azul Systems, Inc.)"
 )
 @Component
--- server/build/generated/sources/annotationProcessor/java/main/ynzmz/server/vote/lecturereview/mapper/LectureReviewVoteMapperImpl.java
+++ server/build/generated/sources/annotationProcessor/java/main/ynzmz/server/vote/lecturereview/mapper/LectureReviewVoteMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-03-14T15:57:40+0900",
+    date = "2023-03-14T23:00:52+0900",
     comments = "version: 1.5.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.6.1.jar, environment: Java 11.0.17 (Azul Systems, Inc.)"
 )
 @Component
