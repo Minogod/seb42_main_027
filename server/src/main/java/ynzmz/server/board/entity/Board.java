fefc2cdec112854299f4f70fb869cb2ce7c73efa
Minogod <93550624+Minogod@users.noreply.github.com>
2023-03-14 23:14:02 +0900

    feat: board table이름수정

--- server/src/main/java/ynzmz/server/board/entity/Board.java
+++ server/src/main/java/ynzmz/server/board/entity/Board.java
@@ -4,15 +4,13 @@ package ynzmz.server.board.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "boards")
 public class Board {
 
     @Id @GeneratedValue
